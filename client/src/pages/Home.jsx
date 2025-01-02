6befbb7e87de9d607b05477f4637be66d0123db5
Sergey87 <serge.techno@gmail.com>
2025-01-02 19:19:49 +0300

    finished home page added filters logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import AppDownload from "../components/AppDownload";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import JobListing from "../components/JobListing";
 import Navbar from "../components/Navbar";
@@ -7,6 +9,8 @@ const Home = () => {
     <Navbar />
     <Hero />
     <JobListing />
+    <AppDownload />
+    <Footer />
   </div>;
 };
 
