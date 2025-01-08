00e1258e184506441ccb32efecb5bfd5a448dab7
Sergey87 <serge.techno@gmail.com>
2025-01-08 23:54:11 +0300

    work with recriuter panel - 2 part

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,68 @@
+import { useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import { JobCategories, JobLocations } from "../assets/assets";
+
 const AddJob = () => {
-    return <div>
-        TEST2
-    </div>;
+
+    const [title, setTitle] = useState('')
+    const [location, setLocation] = useState('Bangalore')
+    const [category, setCategory] = useState('Programming')
+    const [level, setLevel] = useState('Beginner level')
+    const [salary, setSalary] = useState(0)
+
+    const editorRef = useRef(null)
+    const quillRef = useRef(null)
+
+    useEffect(() => {
+        // init quill only once
+
+        if(!quillRef.current && editorRef.current) {
+            quillRef.current = new Quill(editorRef.current, {
+                theme: 'snow'
+            })
+        }
+    }, [])
+
+    return <form>
+        <div>
+            <p>Job Title</p>
+            <input onChange={e => setTitle(e.target.value)} value={title} type="text" placeholder="Type here" required/>
+        </div>
+
+        <div>
+            <p>Job Description</p>
+            <div ref={editorRef}>
+
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <p>Job Category</p>
+                <select onChange={e => setCategory(e.target.value)}>
+                    {JobCategories.map((category, index) => (
+                        <option key={index} value={category}>{category}</option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <p>Job Location</p>
+                <select onChange={e => setLocation(e.target.value)}>
+                    {JobLocations.map((location, index) => (
+                        <option key={index} value={location}>{location}</option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <p>Job Level</p>
+                <select onChange={e => setLevel(e.target.value)}>
+                   <option value="Beginner level">Beginner level</option>
+                   <option value="Intermediate level">Intermediate level</option>
+                   <option value="Senior level">Senior level</option>
+                </select>
+            </div>
+        </div>
+    </form>;
   };
   
   export default AddJob;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Dashboard = () => {
+
+  const navigate = useNavigate()
+
   return <div className="min-h-screen">
 
     <div className="shadow py-4">
       <div className="px-5 flex justify-between items-center">
-        <img className="max-sm:w-32 cursor-pointer" src={assets.logo} alt="img pic" />
+        <img onClick={e => navigate('/')} className="max-sm:w-32 cursor-pointer" src={assets.logo} alt="img pic" />
         <div className="flex items-center gap-3">
           <p className="max-sm:hidden">Welcome, Sergey!</p>
           <div className="relative group">
@@ -18,6 +21,31 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
+      </div>
+    </div>
+
+    <div className="flex items-start">
+
+      {/* left sidebar */}
+      <div className="inline-block min-h-screen border-r-2">
+        <ul className="flex flex-col items-start pt-5 text-gray-800">
+          <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/add-job'}>
+            <img className="min-w-4" src={assets.add_icon} alt="icon pic" />
+            <p className="max-sm:hidden">Add Job</p>
+          </NavLink>
+          <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/manage-jobs'}>
+            <img className="min-w-4" src={assets.home_icon} alt="icon pic" />
+            <p className="max-sm:hidden">Manage Jobs</p>
+          </NavLink>
+          <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/view-applications'}>
+            <img className="min-w-4" src={assets.person_tick_icon} alt="icon pic" />
+            <p className="max-sm:hidden">View Applications</p>
+          </NavLink>
+        </ul>
+      </div>
+
+      <div>
+        <Outlet />
       </div>
     </div>
     
