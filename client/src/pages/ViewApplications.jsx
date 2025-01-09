22c18da903b28900958e0a3ff87b33efe0a07a4f
Sergey87 <serge.techno@gmail.com>
2025-01-09 23:43:14 +0300

    work with recriuter panel - 3 part

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,6 +1,46 @@
+import { assets, viewApplicationsPageData } from "../assets/assets";
+
 const ViewApplications = () => {
     return <div>
-  
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>User name</th>
+                <th>Job Title</th>
+                <th>Location</th>
+                <th>Resume</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {viewApplicationsPageData.map((applicant, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <img src={applicant.imgSrc} alt="img pic" />
+                    <span>{applicant.name}</span>
+                  </td>
+                  <td>{applicant.jobTitle}</td>
+                  <td>{applicant.location}</td>
+                  <td>
+                    <a href="" target="_blank">Resume <img src={assets.resume_download_icon} alt="icon pic" /></a>
+                  </td>
+                  <td>
+                    <div>
+                      <button>...</button>
+                      <div>
+                        <button>Accept</button>
+                        <button>Reject</button>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
     </div>;
   };
   
