762793e82b2c24a8b529324b777cedbbc177ebf6
Sergey87 <serge.techno@gmail.com>
2025-01-12 23:43:39 +0300

    added sentry in project

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
+import './config/instrument.js'
+import * as Sentry from "@sentry/node";
 
 
 const app = express()
+
+// connect db
+await connectDB()
 
 // middlewares
 app.use(cors())
@@ -12,9 +18,15 @@ app.use(express.json())
 
 // routes
 app.get('/', (req, res) => res.send("API WORKING"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  
 
 // PORT
 const PORT = process.env.PORT || 5000
+
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
