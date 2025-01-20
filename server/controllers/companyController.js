f48576a1aaea9b8ca92334f63a9d6a1c19e8d3cc
Sergey87 <serge.techno@gmail.com>
2025-01-20 22:57:21 +0300

    work with company controllers - 4 part

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -2,6 +2,7 @@ import Company from "../models/Company.js";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
+import Job from "../models/Job.js";
 
 // registr new company
 export const registerCompany = async (req, res) => {
@@ -89,15 +90,35 @@ export const getCompanyData = async (req, res) => {};
 // post a new job
 export const postJob = async (req, res) => {
 
-    const { title, description, location, salary } = req.body;
+    const { title, description, location, salary, level, category } = req.body;
 
     const companyId = req.company._id
+    
     // console.log(companyId, {
     //     title, description, location, salary
     // })
 
-    try{}
-    catch(error) {}
+    try{
+
+      const newJob = new Job({
+        title,
+        description,
+        location,
+        salary,
+        companyId,
+        date: Date.now(),
+        level,
+        category
+      })
+
+      await newJob.save()
+
+      res.json({ success: true, newJob})
+    }
+    catch(error) {
+
+      res.json({success: false, message: error.message})
+    }
 };
 
 // get company job applicants
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
+import jobRoutes from './routes/jobRoutes.js'
 
 
 const app = express()
@@ -28,6 +29,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 
 app.post('/webhooks', clerkWebhooks)
 app.use('/api/company', companyRoutes)
+app.use('/api/jobs', jobRoutes)
   
 
 // PORT
