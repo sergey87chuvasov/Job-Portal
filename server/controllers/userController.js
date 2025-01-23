aadc1418c9bf32b8f25a8d8c2c331fb4c67f7237
Sergey87 <serge.techno@gmail.com>
2025-01-23 23:40:17 +0300

    work with company controllers - 6 part

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,63 @@
+import Job from "../models/Job.js"
+import JobApplication from "../models/JobApplication.js"
+import User from "../models/User.js"
 
 
 // get user data
-export const getUserData = async (req, res) => {}
+export const getUserData = async (req, res) => {
+
+    const userId = req.auth.userId
+
+    try {
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.json({ success: false, message: 'User not found'})
+        }
+
+        res.json({ success: true, user})
+    }
+    catch(error) {
+        res.json({ success: false, message: error.message})
+    }
+}
 
 
 // apply for a job
-export const applyForJob = async (req, res) => {}
+export const applyForJob = async (req, res) => {
+
+    const { jobId } = req.body
+    const userId = req.auth.userId
+
+    try {
+
+        const isAlreadyApplied = await JobApplication.find({jobId, userId})
+
+        if (isAlreadyApplied.length > 0) {
+            return res.json({success: false, message: 'Already Applied'})
+        }
+
+        const jobData = await Job.findById(jobId)
+
+        if(!jobData) {
+            return res.json({success: false, message: 'Job Not found'})
+        }
+
+        await JobApplication.create({
+            companyId: jobData.companyId,
+            userId,
+            jobId,
+            date: Date.now()
+        })
+
+        res.json({ success: true, message: 'Applied Successfully'})
+    }
+    catch(error) {
+
+        res.json({ success: false, message: error.message})
+    }
+}
 
 
 // get user applied applictations
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 
 const app = express()
@@ -20,6 +21,7 @@ await connectCloudinary()
 // middlewares
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 
 // routes
